52353f9cdf3c17a2f459818bfa89b56208b2aefc
Luke Primis <luke_primis@brown.edu>
2021-12-05 21:45:00 -0500

    formatting obvservation dict into obvservations space matrix

--- battlefield.py
+++ battlefield.py
@@ -1,5 +1,6 @@
 from pettingzoo.magent import combined_arms_v5
 from numpy import sqrt, square
+import numpy as np
 from time import sleep
 
 VIEW_RADIUS = 5
@@ -37,21 +38,39 @@ def get_agents_in_radius(agent, positions):
                 agents.append(a)
     return agents
 
-def train(env):
+def train(env, names_to_ids, ids_to_names, team_size, team1_class1_model, team1_class2_model, team2_class1_model, team2_class2_model):
     max_cycles = 1000
     observations = env.reset()
     for step in range(max_cycles):
         env.render()
 
-        agent_positions = get_agent_positions(env)
-        focus_agent = env.agents[0]
-        agents_in_radius = get_agents_in_radius(focus_agent, agent_positions)
+        # agent_positions = get_agent_positions(env)
+        # focus_agent = env.agents[0]
+        # agents_in_radius = get_agents_in_radius(focus_agent, agent_positions)
         
-        print("agents in view radius of %s:" % (focus_agent))
-        print(agents_in_radius)
-        sleep(1000)
-        actions = {}
+        # sleep(1000)
+        
+        input_matrix_1 = np.zeros([team_size, 1521])
+        input_matrix_2 = np.zeros([team_size, 1521])
+        
         for agent in env.agents:
+            if TEAM_COLORS[0] in agent:
+                input_matrix_1[names_to_ids[agent] % team_size] = np.reshape(observations[agent], [-1])
+            elif TEAM_COLORS[1] in agent:
+                input_matrix_2[names_to_ids[agent] % team_size] = np.reshape(observations[agent], [-1])
+        actions = {}
+
+        for agent in env.agents:
+            if TEAM_COLORS[0] in agent:
+                if CLASSES[0] in agent:
+                    action = team1_class1_model(input_matrix_1)
+                elif CLASSES[1] in agent:
+                    action = team1_class2_model(input_matrix_1)
+            elif TEAM_COLORS[1] in agent:
+                if CLASSES[0] in agent:
+                    action = team2_class1_model(input_matrix_2)
+                elif CLASSES[1] in agent:
+                    action = team2_class2_model(input_matrix_2)
             obversvations_vector = mask(observations, agent)
             action = Model.call()
             
@@ -109,18 +128,14 @@ def main():
     step_reward=-0.005, dead_penalty=-0.1, attack_penalty=-0.1, 
     attack_opponent_reward=0.2, max_cycles=1000, extra_features=False)
 
-    team_size = env.num_agents / 2
+    team_size = env.num_agents // 2
     agent_names = env.possible_agents
-    print(team_size)
-    print(agent_names)
 
     team1_agents, team2_agents = sort_agents(agent_names)
-    print(team1_agents)
-    print(team2_agents)
 
-    raise Exception('a')
+    names_to_ids, ids_to_names = get_agent_id_maps(agent_names)
 
-    train(env)
+    train(env, names_to_ids, ids_to_names, team_size, None, None, None, None)
 
 
 if __name__ == '__main__':
