42028102990ec9fcd0378db90a6d5dd7ab64d8e7
Avi Trost <avi@avis-mbp.devices.brown.edu>
2021-12-08 19:42:52 -0500

    adjust maps for loss

--- battlefield.py
+++ battlefield.py
@@ -216,7 +216,7 @@ def train(env, id_maps, team_size, team1_model, team2_model):
                     sample_1 = batch_1[j]
                     sample_2 = batch_2[j]
                     for i in range(team_size):
-                        print(sample_1[2])
+                        print(sample_1[6])
                         current_1 = sample_1[2][i]
                         discounted_1 = (1-sample_1[6][id_maps[TEAM_COLORS[0]]['ids_to_names'][i]])*GAMMA*target_q_values_1[j][i]
                         expected_q_values_1[j][i][sample_1[1][id_maps[TEAM_COLORS[0]]['ids_to_names'][i]]] = current_1 + discounted_1
